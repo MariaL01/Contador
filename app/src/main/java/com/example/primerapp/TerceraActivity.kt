69aeffb0494057f6b0e01e907c9d2c9d18372be2
MariaL01 <lupiita069@gmail.com>
2021-04-21 15:17:47 -0500

    Initial commit

---
+++ app/src/main/java/com/example/primerapp/TerceraActivity.kt
@@ -0,0 +1,28 @@
+package com.example.primerapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.content.Intent
+import android.view.View
+import android.widget.Button
+
+class TerceraActivity : AppCompatActivity() {
+    //Declaración de Variables
+    lateinit var btnInicio2:Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tercera)
+
+        //Inicialización
+        btnInicio2 = findViewById(R.id.btnInicio2)
+
+        btnInicio2.setOnClickListener(View.OnClickListener {
+            val miIntent4 = Intent(this, MainActivity::class.java)
+            startActivity(miIntent4)
+
+        })
+
+
+    }
+}
